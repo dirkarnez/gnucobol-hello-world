d70cf8132dd82dee9117046516a2fc1a8293d20c
Dirk Arnez <smalldirkalex@gmail.com>
2024-12-05 04:16:17 +0800

    - Update local-build.cmd

--- local-build.cmd
+++ local-build.cmd
@@ -2,8 +2,8 @@
 
 set GNUCOB_MAIN_DIR=%USERPROFILE%\Downloads\gnucobol-portable-v3.2
 
-set COB_CFLAGS=-I"%GNUCOB_MAIN_DIR%\include" %COB_CFLAGS%
-set COB_LDFLAGS=-L"%GNUCOB_MAIN_DIR%\lib" %COB_LDFLAGS%
+set "COB_CFLAGS=-I"%GNUCOB_MAIN_DIR%\include" %COB_CFLAGS%"
+set "COB_LDFLAGS=-L"%GNUCOB_MAIN_DIR%\lib" -static %COB_LDFLAGS%"
 
 set PATH=^
 %GNUCOB_MAIN_DIR%;^
