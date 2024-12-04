cb718a8b1c58b1c0357c0fdf650d075923944915
Dirk Arnez <smalldirkalex@gmail.com>
2024-12-05 03:46:47 +0800

    - update local-build.cmd

--- local-build.cmd
+++ local-build.cmd
@@ -23,6 +23,6 @@
 @REM   set "TZDIR=%COB_MAIN_DIR%\share\zoneinfo"
 @REM )
 
-%USERPROFILE%\Downloads\GnuCOBOL\bin\cobc.exe -x --verbose main.cob
+%USERPROFILE%\Downloads\gnucobol-portable-v3.2\bin\cobc.exe -x --verbose main.cob
 @REM %WINDIR%\system32\robocopy "C:\Users\19081126D\Downloads\gnucobol-3.3-dev-MinGW-binaries (debug)\bin" "%cd%" libgcc_s_dw2-1.dll libgmp-10.dll libiconv-2.dll libintl-8.dll libpdcurses.dll libcob-4.dll libdb-6.0.dll
 pause
